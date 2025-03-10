afe6dcb0092a198833de8a51530a7044f2576410
satyaa232323 <attalasatya@gmail.com>
2025-03-10 11:45:44 +0700

    Refactor authentication controllers to return JSON responses; update routes for login and registration

--- Api-Gudang/routes/api.php
+++ Api-Gudang/routes/api.php
@@ -1,38 +1,39 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Resources\CategoryResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Route::apiResource(name: '/products', controller: ProductController::class)->only([
+//     'index', 'show'
+// ]);
 
 Route::middleware(['auth:sanctum'])->group( function () {
     
     // auth
-
-
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+    
     // endauth
-
+    
+    // product
+     Route::apiResource(name: '/products', controller: ProductController::class);
+     Route::put('/productUpdate/{id}', [ProductController::class, 'update']);
+     Route::delete('/productDelete/{id}', [ProductController::class, 'destroy']);
+    // endproduct
+    
+    //  categories
+     Route::apiResource(name: '/categories', controller: CategoryController::class);
+     Route::put('/categoriesUpdate/{id}', action: [CategoryController::class, 'update']);
+     Route::delete('/categoriesDelete/{id}', action: [CategoryController::class, 'destroy']);
+    //  end categories
+    
 
     // categories 
 
     //  end product
-    
-    
-    
 });
-Route::apiResource(name: '/categories', controller: CategoryController::class);
-Route::put('/category/{id}', [CategoryResource::class, 'update']);
-Route::delete('/category/{id}', [CategoryResource::class, 'destroy']);
 
-// product
- Route::apiResource(name: '/products', controller: ProductController::class);
- Route::put('/productUpdate/{id}', [ProductController::class, 'update']);
- Route::delete('/productDelete/{id}', [ProductController::class, 'destroy']);
-// endproduct
-
-//  categories
- Route::apiResource(name: '/categories', controller: CategoryController::class);
- Route::put('/categoriesUpdate/{id}', action: [CategoryController::class, 'update']);
- Route::delete('/categoriesDelete/{id}', action: [CategoryController::class, 'destroy']);
-//  end categories
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
